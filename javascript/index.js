0d207ee250c2ba8d6735bce9ddb6c2b941022fb4
kh-rasha <rasha.alkhateeb97@gmail.com>
2026-01-13 12:10:50 +0100

    kopplat js

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,28 @@
-// javascript/main.js
 import { posts } from "./data.js";
 
 const postsSection = document.querySelector("#posts");
 if (!postsSection) {
-    throw new Error('Element with id="posts" not found in index.html');
+    throw new Error('Element with id="posts" not found');
 }
 
 postsSection.innerHTML = posts
     .map(
         (post) => `
       <article class="post-card">
-        <div class="thumb" aria-hidden="true"></div>
+        <img 
+          class="thumb"
+          src="../${post.image}" 
+          alt="${post.title}"
+        />
+
         <div>
           <h3>${post.title}</h3>
           <p>${post.excerpt}</p>
-          <a class="read-more" href="post.html?id=${encodeURIComponent(post.id)}">Read more</a>
+          <a class="read-more" href="post.html?id=${encodeURIComponent(post.id)}">
+            Read more
+          </a>
         </div>
       </article>
     `
     )
     .join("");
-
