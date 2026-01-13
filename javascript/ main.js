1f6753af8a22587b0ed8e181e073ffa0af702e45
kh-rasha <rasha.alkhateeb97@gmail.com>
2026-01-13 10:13:18 +0100

    Add  6 posts to data.js

---
+++ javascript/ main.js
@@ -0,0 +1,20 @@
+// js/main.js
+import { posts } from "./data.js";
+
+const postsSection = document.querySelector("#posts");
+
+if (!postsSection) {
+    throw new Error("Element #posts not found in index.html");
+}
+
+postsSection.innerHTML = posts
+    .map(
+        (post) => `
+      <article class="post-card">
+        <h2>${post.title}</h2>
+        <p>${post.excerpt}</p>
+        <a href="post.html?id=${encodeURIComponent(post.id)}">Read more</a>
+      </article>
+    `
+    )
+    .join("");
